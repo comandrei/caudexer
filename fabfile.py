10e6d9560e398a4aae7877f8e5be931e00d5d61d
Andrei Coman <andrei.coman@3pillarglobal.com>
2015-11-07 11:56:33 +0200

    Add nginx and gunicorn config files and step

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
-from fabric.api import env, run
+from fabric.api import env, run, put
 # the user to use for the remote commands
 env.user = 'caudexer'
 # the servers where the commands are executed
 env.hosts = ['caudexer.com']
 REPO_TO_CLONE = "https://github.com/comandrei/caudexer/"
+
 def provision():
     deps = []
     with open("caudexer/requirements.apt") as req_file:
@@ -14,9 +15,17 @@ def provision():
     run("sudo apt-get update")
     run("sudo apt-get install {} --assume-yes".format(" ".join(deps)))
 
+def configure():
+    put("caudexer.nginx.conf", "/tmp/caudexer.nginx.conf")
+    put("gunicorn.conf", "/tmp/gunicorn.conf")
+    run("sudo mv /tmp/gunicorn.conf /etc/init/gunicorn.conf")
+    run("sudo mv /tmp/caudexer.nginx.conf /etc/nginx/sites-enabled/caudexer.conf")
+    run("sudo service nginx restart")
 
 def deploy():
     run("rm -rf deployed")
+    run("rm -rf caudexer")
     run("virtualenv deployed --python=$(which python3)")
     run("git clone {}".format(REPO_TO_CLONE))
-    run("deployed/bin/pip install -r caudexer/caudexer/requirements")
+    run("deployed/bin/pip install -r caudexer/caudexer/requirements.txt")
+    run("sudo service gunicorn restart")
